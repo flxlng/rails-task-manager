693fa915795edf70a224829580e59858fcafa37f
Felix Lange <rayon-zunder.0u@icloud.com>
2024-02-13 16:05:33 +0900

    todos sind am todoen

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,7 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+    # @task.save
     if @task.save
       redirect_to task_path(@task)
     else
@@ -21,7 +22,28 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,13 @@ Rails.application.routes.draw do
   get "/task/new", to: "tasks#new", as: :new_task
   post "/tasks", to: "tasks#create"
 
+  #update
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "/tasks/:id", to: "tasks#update"
+
+  #delete
+  delete "/tasks/:id", to: "tasks#destroy"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
