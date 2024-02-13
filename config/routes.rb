916bbf20aaf1eb869bb6cade6be2f7115b98b352
Felix Lange <rayon-zunder.0u@icloud.com>
2024-02-13 13:40:40 +0900

    CR

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  #read all
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+  #read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  #create
+  get "/task/new", to: "tasks#new", as: :new_task
+  post "/tasks", to: "tasks#create"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
